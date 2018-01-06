3fecaf27f154dbe7263e2d4f0b4835f25c788455
Mark Velednitsky <marvel2010+github@gmail.com>
2018-01-05 17:15:57 -0800

    utility to combine several vertices

--- combined_vertices.py
+++ combined_vertices.py
@@ -1,8 +1,30 @@
-def combined_vertices(G, u, v):
+"""Utilities for combining vertices."""
+
+
+def combined_vertices_several(graph, u, v_set):
+    """Combines several vertices"""
+    H = graph.copy()
+    for v in v_set:
+        H = combined_vertices(H, u, v)
+    return H
+
+
+def combined_vertices(graph, u, v):
+    """
+    Combined two nodes in a graph such that their outgoing capacities sum.
+
+    Input:
+        graph
+        u
+        v
+
+    Output:
+        new_graph
+    """
     # copy graph
-    H = G.copy()
+    H = graph.copy()
     # decide if nodes are single or duplicate
-    for x, w, d in G.edges(v, data=True):
+    for x, w, d in graph.edges(v, data=True):
         if u == w:
             continue
         if (u, w) in H.edges(u):
