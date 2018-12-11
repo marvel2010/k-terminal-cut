7e7bf9fb76e18a8f6014917cf99f026c0fa0de93
Mark Velednitsky <marvel2010+github@gmail.com>
2018-12-10 16:07:07 -0800

    added documentation for minimum isolating cut

--- src/ktcut/minimum_isolating_cut.py
+++ src/ktcut/minimum_isolating_cut.py
@@ -21,21 +21,28 @@ def minimum_isolating_cut(graph, source_nodes, sink_nodes):
         cut_weight: the weight of the isolating cut
     """
 
-    # construct aux graph
+    # construct auxiliary graph with super-source and super-sink nodes
     graph.add_nodes_from(['s_node', 't_node'])
     graph.add_edges_from([('s_node', source_adj_node) for source_adj_node in source_nodes])
     graph.add_edges_from([(sink_adj_node, 't_node') for sink_adj_node in sink_nodes])
 
-    # find minimum cut with maximal source set
-    residual = preflow_push(graph, 's_node', 't_node')  # residual graph
+    # find the residual graph after running a maximum flow algorithm
+    residual = preflow_push(graph, 's_node', 't_node')
 
-    # use >= to deal with float issue
+    # remove the edges which are saturated in the residual graph
     cutset = [(u, v, d) for u, v, d in residual.edges(data=True) if d['flow'] >= d['capacity']]
     residual.remove_edges_from(cutset)
+
+    # the sink set is all nodes which are reachable from the super-sink
+    #   after the saturated arcs have been removed
     cut_sink = set(nx.shortest_path_length(residual, target='t_node'))
+    # the source set is all the nodes which are not in the sink set
     cut_source = set(graph) - cut_sink
+
     if cutset is not None:
         residual.add_edges_from(cutset)
+
+    # determine the weight of the resulting minimum cut
     cut_weight = residual.graph['flow_value']
 
     assert 's_node' in cut_source, ' source node not included in source set '
